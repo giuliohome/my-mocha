19ef05bdd488a815a94fa286febd9b285c96873f
Giulio <giuliohome@gmail.com>
2023-02-27 21:24:10 +0100

    Skipping tests programmatically at runtime

--- test/test.js
+++ test/test.js
@@ -1,8 +1,22 @@
 var assert = require('assert');
+const branchName = require('current-git-branch');
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
+});
+
+describe('Dev Env', function() {
+    it('should run only on dev env', function () {
+        const branch = branchName();
+        if (branch === 'dev') {
+            // assertions
+            assert.equal(branch,'dev');s
+        } else {
+            this.skip();
+        }
+    });
 });
